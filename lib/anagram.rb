a958c2af66ed95d2e2591153f1d8ccabfcc96080
elisa <elisaopalka@gmail.com>
2020-11-19 15:06:34 -0500

    commit tree d9c74f6ea211a9f14ad4d4e3ae349af4e5077951

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+      def initialize(word)
+        @word = word
+      end
+  
+      def match(candidates)
+        matching_letters = letters(@word)
+        anagrams = candidates.select do |candidate|
+          letters(candidate) == matching_letters
+        end
+        anagrams - [@word]
+      end
+
+      def letters(word)
+        word.to_s.downcase.chars.sort
+      end
+      private :letters
+  end
